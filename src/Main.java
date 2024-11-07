ef076cc65210490e1c948e2855e436285099c0fb
iSkySan <theofragale@hotmail.fr>
2024-11-07 10:09:53 +0100

    Chaine of responsibility correctv2

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,4 @@
 import Chain_of_Responsibility.*;
 
 public class Main {
-    public static void main(String[] args) {
-       CentreAppel centreAppel1 = new CentreAppelLevel1();
-       CentreAppel centreAppel2 = new CentreAppelLevel2();
-       CentreAppel centreAppel3 = new CentreAppelLevel3();
-       CentreAppel centreAppel4 = new CentreAppelLevel4();
-
-        centreAppel1.setNext(centreAppel2);
-        centreAppel2.setNext(centreAppel3);
-        centreAppel3.setNext(centreAppel4);
-
-
-        Appel appel = new Appel(1, "Probleme de connexion");
-        centreAppel1.traiterAppel(appel);
-
-        appel = new Appel(2, "Probleme de facturation");
-        centreAppel1.traiterAppel(appel);
-
-        appel = new Appel(3, "Probleme de electricite");
-        centreAppel1.traiterAppel(appel);
-        appel = new Appel(4, "oskour");
-        centreAppel1.traiterAppel(appel)
-
-
-
-       ;
-    }
-
 }
